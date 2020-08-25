941a36fe2b1850cbe3d92969f4352f4dba491654
vngarg <sngargrsd@gmail.com>
2020-08-25 23:57:47 +0530

    Added Icons

--- client/src/Pages/Projects/Projects.js
+++ client/src/Pages/Projects/Projects.js
@@ -6,6 +6,9 @@ import { Container, Row, Col } from 'reactstrap';
 import './Projects.css';
 import ProjectCards from '../../components/ProjectCards/ProjectCards';
 
+import TestIcon from '../../assets/Icons/TestLogo.png';
+import HTML from '../../assets/Icons/HTML.png';
+
 const Projects = () => {
     return (
         <div>
@@ -14,14 +17,27 @@ const Projects = () => {
                 <h1 className='mt-5 mb-5'>My Projects</h1>
                 <Row>
                     <Col lg={6} sm={12}>
+                        <ProjectCards projectIcon={TestIcon} projectName='GitHub Finder' description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." projectLink='https://githubfinder142.netlify.com/' emoji1='🔥' Logo={HTML} />
+                    </Col>
+                    <Col lg={6} sm={12}>
                         <ProjectCards />
                     </Col>
-                    {/* <Col lg={4} sm={12}>
+                </Row>
+                <Row>
+                    <Col lg={6} sm={12}>
                         <ProjectCards />
                     </Col>
-                    <Col lg={4} sm={12}>
+                    <Col lg={6} sm={12}>
                         <ProjectCards />
-                    </Col> */}
+                    </Col>
+                </Row>
+                <Row>
+                    <Col lg={6} sm={12}>
+                        <ProjectCards />
+                    </Col>
+                    <Col lg={6} sm={12}>
+                        <ProjectCards />
+                    </Col>
                 </Row>
             </Container>
             <Footer />
