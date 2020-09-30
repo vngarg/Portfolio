ca4359dcd28b9e51b8598617190cb8a95bfe2b1d
vngarg <sngargrsd@gmail.com>
2020-10-01 03:49:56 +0530

    Hero Section ALmost Done

--- client/src/components/HeroSection/Hero.js
+++ client/src/components/HeroSection/Hero.js
@@ -1,5 +1,5 @@
-import React, { Component, Fragment } from "react";
-import { Row, Col, Button, Container } from "reactstrap";
+import React, { Component } from "react";
+import { Row, Col, Button } from "reactstrap";
 import Particles from "react-particles-js";
 
 import "./Hero.css";
@@ -8,26 +8,22 @@ import Shlok from "../../assets/images/Shlok.jpg";
 class Hero extends Component {
   render() {
     return (
-      <Fragment>
         <div className="Hero">
-          <Container>
             <Row className="m-0">
-              <Col xl={6} sm={12}>
+              <Col xl={6} sm={12} className='text-center ImageDiv'>
                 <img src={Shlok} alt="Shlok" className="HeroImage" />
               </Col>
               <Col xl={6} sm={12} className="text-left">
                 <div className="Heading">Hey, I'm Shlok Garg.</div>
                 <div className="HeroText">
-                  A MERN Stack developer and an Android developer from New
+                  I'm a MERN Stack developer and an Android developer from New
                   Delhi, India. I create appealing websites and Android apps.
                 </div>
                 <br />
                 <Button className="button">My Projects</Button>
               </Col>
             </Row>
-          </Container>
-          <Particles className='particles'
-          width='98.5vw'
+            <Particles className='particles'
             params={{
               particles: {
                 number: {
@@ -41,7 +37,6 @@ class Hero extends Component {
             }}
           />
         </div>
-      </Fragment>
     );
   }
 }
