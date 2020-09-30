6e93b7f028a5585bd5acdaca794ffb32e4597a40
vngarg <sngargrsd@gmail.com>
2020-10-01 00:07:43 +0530

    Adde HeroSection

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Contact from './ContactMe/Contact';
+import Hero from './components/HeroSection/Hero';
 
 function App() {
   return (
     <div className="App">
-      <Contact />
+      <Hero />
     </div>
   );
 }
