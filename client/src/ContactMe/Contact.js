bc9ec4bd223b97c7ae84e5d37e32202ee056b313
vngarg <sngargrsd@gmail.com>
2020-09-30 16:58:25 +0530

    Added Slant Hero Section

---
+++ client/src/ContactMe/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from 'react';
+
+import './Contact.css';
+
+class Contact extends Component {
+    render() { 
+        return (
+            <Fragment>
+                <div className='ContactForm'>
+                    HELLO WORLD
+                </div>
+            </Fragment>
+        );
+    }
+}
+ 
+export default Contact;
---
+++ client/src/components/Navbar/Menu.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react';
+
+class Menu extends Component {
+    state = {  }
+    render() { 
+        return (
+            <Fragment>
+                This is Menu
+            </Fragment>
+        );
+    }
+}
+ 
+export default Menu;
