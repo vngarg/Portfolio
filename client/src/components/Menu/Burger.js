9f095fdcafaa519e2c95f31c19025020178d4a97
vngarg <sngargrsd@gmail.com>
2020-10-01 17:05:59 +0530

    Menu almost done

--- client/src/components/Menu/Burger.js
+++ client/src/components/Menu/Burger.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 
 const Burger = () => {
+    const [open, SetOpen] = useState(false);
 
     const style = {
         "width": "2rem",
@@ -13,18 +14,41 @@ const Burger = () => {
         "flex-flow": "column nowrap",
     }
 
-    const Burgerbar = {
+    const Burgerbar1 = {
         "width": "2rem",
         "height": "0.25rem",
-        "background-color": "black",
+        "background-color": open ? "green" : "black",
         "border-radius": "20px",
+        "transform-origin": "1px",
+        "transform": open ? "rotate(45deg)" : "rotate(0deg)",
+        "transition": "all 0.3s linear",
+    }
+
+    const Burgerbar2 = {
+        "width": "2rem",
+        "height": "0.25rem",
+        "background-color": open ? "green" : "black",
+        "border-radius": "20px",
+        "transform-origin": "1px",
+        "transform": open ? "translateX(100%)" : "translateX(0)",
+        "opacity": open ? '0' : '1',
+    }
+
+    const Burgerbar3 = {
+        "width": "2rem",
+        "height": "0.25rem",
+        "background-color": open ? "green" : "black",
+        "border-radius": "20px",
+        "transform-origin": "1px",
+        "transform": open ? "rotate(-45deg)" : "rotate(0)",
+        "transition": "all 0.3s linear",
     }
 
     return (
-        <div className='Burger' style={style}>
-            <div style={Burgerbar} />
-            <div style={Burgerbar} />    
-            <div style={Burgerbar} />
+        <div className='Burger' style={style} open={open} onClick={() => SetOpen(!open)}>
+        <div style={Burgerbar1} />
+            <div style={Burgerbar2} />    
+            <div style={Burgerbar3} />
         </div>
     )
 }
