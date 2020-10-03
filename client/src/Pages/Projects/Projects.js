ae4fc9f4c86a56d63bf4047dce68d5b491ed251c
vngarg <sngargrsd@gmail.com>
2020-10-03 20:04:39 +0530

    Started Projects

--- client/src/Pages/Projects/Projects.js
+++ client/src/Pages/Projects/Projects.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import Menu from '../../components/Nav/Navbar';
+import Footer from '../../components/Footer/Footer';
+import { Container, Row, Col } from 'reactstrap';
+
+import './Projects.css';
+import ProjectCards from '../../components/ProjectCards/ProjectCards';
 
 const Projects = () => {
     return (
         <div>
-            <center>
-                You are now on Projects Page.
-            </center>
+            <Menu />
+            <Container>
+                <h1 className='mt-5'>My Projects</h1>
+                <Row>
+                    <Col lg={4} sm={12}>
+                        <ProjectCards />
+                    </Col>
+                    <Col lg={4} sm={12}>
+                        <ProjectCards />
+                    </Col>
+                    <Col lg={4} sm={12}>
+                        <ProjectCards />
+                    </Col>
+                </Row>
+            </Container>
+            <Footer />
         </div>
     )
 }
---
+++ client/src/components/ProjectCards/ProjectCards.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProjectCards = () => {
+    return (
+        <div>
+            <center>These are the project cards.</center>
+        </div>
+    )
+}
+
+export default ProjectCards
