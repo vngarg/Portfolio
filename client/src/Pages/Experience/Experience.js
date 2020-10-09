355a50db2d8c186a0ea10367b64d11004ab24838
vngarg <sngargrsd@gmail.com>
2020-10-09 14:12:01 +0530

    Completed Experiencec section for desktop

--- client/src/Pages/Experience/Experience.js
+++ client/src/Pages/Experience/Experience.js
@@ -4,12 +4,14 @@ import Footer from "../../components/Footer/Footer";
 import { Container } from "reactstrap";
 import ExperienceCard from "../../components/ExperienceCard/ExperienceCard";
 
+import Gordian from '../../assets/Logos/Gordian.png';
+
 const Experience = () => {
   return (
     <div>
       <Menu />
       <Container>
-          <ExperienceCard position='MERN Stack developer' CompanyName='Gordian Technologies Pvt. Ltd.' desc='Will enter later' start='August 2020' end='present' duration='present' Location='Work from home' CredentialLink='https://www.linkedin.com/in/shlok-garg-0b216b191/' />
+          <ExperienceCard Logo={Gordian} position='MERN Stack developer' CompanyName='Gordian Technologies Pvt. Ltd.' desc='Managing all the web developers in the organisation is a great work. I have to clear their doubts and also they all report directly to me for all the changes needed in the website. Currently I am developing a new website for the organisation.' start='August 2020' end='present' duration='3 months' Location='Work from home' CredentialLink='https://www.linkedin.com/in/shlok-garg-0b216b191/' />
       </Container>
       <Footer />
     </div>
--- client/src/components/ExperienceCard/ExperienceCard.js
+++ client/src/components/ExperienceCard/ExperienceCard.js
@@ -1,36 +1,39 @@
-import React from 'react'
-import { Row, Col } from 'reactstrap';
+import React from "react";
+import { Row, Col } from "reactstrap";
 
-import './cards.css';
+import "./cards.css";
 
 const ExperienceCard = (props) => {
-    return (
-        <div>
-            <Row>
-                <Col xl={6} sm={12}>
-                    <div className='position'>
-                        {props.position}
-                    </div>
-                </Col>
-                <Col xl={6} sm={12}>
-                    <div className='CompanyName'>
-                        {props.CompanyName}
-                    </div>
-                    <div className='duration'>
-                        {props.start}-{props.end}{' '}{props.duration}
-                    </div>
-                    <div className='location'>
-                        {props.Location}
-                    </div>
-                    <div className='Workdescription'>
-                        {props.desc}
-                    </div>
+  return (
+    <div>
+      <Row className='row'>
+        <Col xl={6} sm={12}>
+          <div className="position">{props.position}</div>
+        </Col>
+        <Col xl={6} sm={12}>
+          <div className="CompanyName">
+            <img src={props.Logo} alt="Gordian Technologies" className="Logo" />
+            {props.CompanyName}
+          </div>
+          <div className="duration">
+            {props.start} - {props.end}, {props.duration}
+          </div>
+          <div className="location">{props.Location}</div>
+          <div className="Workdescription">{props.desc}</div>
 
-                    Enter credential link, if present.
-                </Col>
-            </Row>
-        </div>
-    )
-}
+          <a
+            href={props.CredentialLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="credential"
+          >
+            Certificate
+          </a>
+        </Col>
+      </Row>
+      <hr />
+    </div>
+  );
+};
 
-export default ExperienceCard
+export default ExperienceCard;
