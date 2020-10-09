f8a85391baf0734294fb1a2cd49cbdee67bcbe5f
vngarg <sngargrsd@gmail.com>
2020-10-09 15:19:15 +0530

    Added more experience

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 - [ ] Logos in Skills on About Page and color thme as a shade of blue matching with theme.
 - [ ] ::after effect in navbar on mobile version. 
 - [ ] Try to make top image hexagon.
+- [ ] Add technology section in experience.
+- [ ] Experience m check for the duration.
 - [ ] Add right and left wala aos effect to experience section. Left wala left s and right wala right s.
 - [x] Instagram Logo.
 - [ ] Correctly set the Contact and Email in footer on desktop view.
--- client/src/Pages/Experience/Experience.js
+++ client/src/Pages/Experience/Experience.js
@@ -6,6 +6,10 @@ import ExperienceCard from "../../components/ExperienceCard/ExperienceCard";
 
 import Gordian from '../../assets/Logos/Gordian.png';
 import SKCF from '../../assets/Logos/SKCF.jpg';
+import YuMedic from '../../assets/Logos/YuMedic.png';
+import GirlScript from '../../assets/Logos/GirlScript.jpeg';
+import Codebox from '../../assets/Logos/Codebox.jpg';
+import VKarma from '../../assets/Logos/VKarma.png';
 
 const Experience = () => {
 
@@ -38,12 +42,42 @@ const Experience = () => {
     )
   }
 
+  const YuMedicDesc = () => {
+    return (
+      <div>
+        YuMedic is a company that promotes medical facilites in India and are working to bring the medical sector online, just a click away. I worked with them for a month and developed
+        <ol>
+          <li>Various Pages of their website.</li>
+          <li>Modified some previous pages as well.</li>
+          <li>Helped the team with the UI of the website and suggested some great ones.</li>
+        </ol>
+      </div>
+    )
+  }
+
+  const VKarmaDesc = () => {
+    return (
+      <div>
+        I developed an android app which was based on School Management System. Developing the entire app from scratch and all alone I learnt a lot. Main features that I implemented are 
+        <ol>
+          <li>Added Login and Signup feature.</li>
+          <li>Added Spalsh Screen.</li>
+          <li>Implemented attractive and catchy UI.</li>
+          <li>Created more than 10 screens each one with different functionality.</li>
+          <li>Integrated various python APIs for the backend part.</li>
+        </ol>
+      </div>
+    )
+  }
+
   return (
     <div>
       <Menu />
       <Container>
-          <ExperienceCard Logo={Gordian} position='MERN Stack developer' CompanyName='Gordian Technologies Pvt. Ltd.' desc={GordianDesc()} start='August 2020' end='present' duration='3 months' Location='Work from home' CredentialLink='null' websiteLink='https://orders.gordian.in' />
+          <ExperienceCard Logo={Gordian} position='MERN Stack developer' CompanyName='Gordian Technologies Pvt. Ltd.' desc={GordianDesc()} start='August 2020' end='present' duration='null' Location='Work from home' CredentialLink='null' websiteLink='https://orders.gordian.in' />
           <ExperienceCard Logo={SKCF} position='Web development Domain Head' CompanyName='SK Children Foundation' desc={SKDesc()} start='March 2020' end='present' duration='null' Location='Work from home' CredentialLink='https://drive.google.com/file/d/1CNBYPZ2PdZb6x5My4AvUN9BE4ii04dnK/view?usp=sharing' websiteLink='https://skchildrenfoundation.com/' />
+          <ExperienceCard Logo={YuMedic} position='Front-end React developer' CompanyName='YuMedic Pvy. Ltd.' desc={YuMedicDesc()} start='June 2020' end='July 2020' duration='1 month' Location='Work from Home' CredentialLink='https://drive.google.com/file/d/1omt6P2jVf_-a2uaFot0vT9Bp2NTjzacC/view?usp=sharing' websiteLink='http://yumedic.com/' />
+          <ExperienceCard Logo={VKarma} position='Android developer' CompanyName='VKarma Pvt. Ltd.' desc={VKarmaDesc()} start='May 2020' end='July 2020' duration='3 months' Location='Work from home' CredentialLink='https://drive.google.com/file/d/1htUaDDEqipASn-JRLQ64alRbUI3T65SI/view?usp=sharing' websiteLink='https://drive.google.com/file/d/1QUP2K7r45PXQWAeVvh8_Oqy8NIVnvYpS/view?usp=sharing' />
       </Container>
       <Footer />
     </div>
