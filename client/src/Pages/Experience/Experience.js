7ae5b0346e3fbebd06eec19313cab8715ffdc9b7
vngarg <sngargrsd@gmail.com>
2020-10-09 13:27:21 +0530

    Started Experience Cards

--- client/src/Pages/Experience/Experience.js
+++ client/src/Pages/Experience/Experience.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from "react";
+import Menu from "../../components/Nav/Navbar";
+import Footer from "../../components/Footer/Footer";
+import { Container } from "reactstrap";
+import ExperienceCard from "../../components/ExperienceCard/ExperienceCard";
 
 const Experience = () => {
-    return (
-        <div>
-            <center>
-                You are now on experience page.
-            </center>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Menu />
+      <Container>
+          <ExperienceCard />
+      </Container>
+      <Footer />
+    </div>
+  );
+};
 
-export default Experience
+export default Experience;
---
+++ client/src/components/ExperienceCard/ExperienceCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExperienceCard = (props) => {
+    return (
+        <div>
+            This is the experience card.
+        </div>
+    )
+}
+
+export default ExperienceCard
