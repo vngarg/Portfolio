d705077add662433506f3e34678aec86b2e93a6c
vngarg <sngargrsd@gmail.com>
2020-10-14 02:01:03 +0530

    Certifications almost done

--- client/src/components/Certifications/Certification.js
+++ client/src/components/Certifications/Certification.js
@@ -1,18 +1,21 @@
-import React from 'react'
-import { Row, Col } from 'reactstrap';
+import React from "react";
+import { Button } from 'reactstrap';
 
-import './style.css';
+import "./style.css";
+
+import Image from "../../assets/images/Projects/Anugoonj.png";
 
 const Certification = () => {
-    return (
-        <div className='Certi'>
-            <Row>
-                <Col lg={4} sm={12}>
-                    HELLO WORLD
-                </Col>
-            </Row>
-        </div>
-    )
-}
+  return (
+    <div className="Certi text-center">
+      <img src={Image} alt="Logo" className="logo" />
+      <h2 className='mb-3'>Certificate Title</h2>
+      <p className='mr-4 ml-4'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+      <Button color='primary' style={{ 'borderRadius': '13px' }}>
+        Certification
+      </Button>
+    </div>
+  );
+};
 
-export default Certification
+export default Certification;
