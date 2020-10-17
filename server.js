5b2c158887dcbcd5593ab5397b68c4e712b176a1
vngarg <sngargrsd@gmail.com>
2020-10-17 13:37:26 +0530

    Working on Heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 require("dotenv").config();
 const nodemailer = require('nodemailer');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT;
@@ -77,6 +78,10 @@ app.post("/Response", (req, res) => {
 // For deployment to Heroku
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
 }
 
 app.listen(PORT, console.log(`Server is started at port ${PORT}`));
