c591d8a725229e4361203e684616305b4534f857
vngarg <sngargrsd@gmail.com>
2020-10-17 23:32:24 +0530

    Updates

---
+++ precache-manifest.1030a957fd728c726e037617cf6fb5b7.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3f0565c6adc29a72db3567a3fb1631",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "0ec9fbd28a36e81f3f80",
+    "url": "/Portfolio/static/css/2.50a5f423.chunk.css"
+  },
+  {
+    "revision": "ed866563ff6463bc9511",
+    "url": "/Portfolio/static/css/main.45824207.chunk.css"
+  },
+  {
+    "revision": "0ec9fbd28a36e81f3f80",
+    "url": "/Portfolio/static/js/2.eec12d85.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/Portfolio/static/js/2.eec12d85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed866563ff6463bc9511",
+    "url": "/Portfolio/static/js/main.e2fe8ece.chunk.js"
+  },
+  {
+    "revision": "efed155d89bc85b81984",
+    "url": "/Portfolio/static/js/runtime-main.88fabfc4.js"
+  },
+  {
+    "revision": "eb4557329de1608e17c1898a471e1806",
+    "url": "/Portfolio/static/media/API.eb455732.jpg"
+  },
+  {
+    "revision": "e54f91142f4ab518e002b0b9b3e1e2bf",
+    "url": "/Portfolio/static/media/Anugoonj.e54f9114.webp"
+  },
+  {
+    "revision": "21fabd9c25168b2d44ab74438d6499ea",
+    "url": "/Portfolio/static/media/COVID.21fabd9c.png"
+  },
+  {
+    "revision": "17c05515436bdb69c823648e9e2362b2",
+    "url": "/Portfolio/static/media/COdingNinjas.17c05515.jpg"
+  },
+  {
+    "revision": "e73d4b8ab772cbadb5245ad41e934676",
+    "url": "/Portfolio/static/media/CP.e73d4b8a.jpg"
+  },
+  {
+    "revision": "9f8ecef4a0e12488af0249df24680a4a",
+    "url": "/Portfolio/static/media/CodePlayer.9f8ecef4.png"
+  },
+  {
+    "revision": "35d51bf2a065047db1162fb13b915029",
+    "url": "/Portfolio/static/media/Codebox.35d51bf2.jpg"
+  },
+  {
+    "revision": "0e7f4e81e193a7cb1bab48b08d9d1113",
+    "url": "/Portfolio/static/media/Coursera.0e7f4e81.png"
+  },
+  {
+    "revision": "8f5ce27564945d2c9a10ef827549a78c",
+    "url": "/Portfolio/static/media/Facebook.8f5ce275.png"
+  },
+  {
+    "revision": "39467d0f475eabfd3def899aff62d8e3",
+    "url": "/Portfolio/static/media/Flutter.39467d0f.webp"
+  },
+  {
+    "revision": "c46d88dc57b22678b37d5acf5fbdbab6",
+    "url": "/Portfolio/static/media/Game.c46d88dc.png"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "/Portfolio/static/media/GitHubFinder.0ba2aa20.png"
+  },
+  {
+    "revision": "f00b334e60b029b6c9aec7d099484649",
+    "url": "/Portfolio/static/media/Gmail.f00b334e.png"
+  },
+  {
+    "revision": "404a8307069d3e9e888b8080dee4de30",
+    "url": "/Portfolio/static/media/Gordian.404a8307.png"
+  },
+  {
+    "revision": "3c5c603f09236a200062b7d295d43f80",
+    "url": "/Portfolio/static/media/HTML.3c5c603f.png"
+  },
+  {
+    "revision": "f44eb2736e776e5dacd566b2ea45fa1f",
+    "url": "/Portfolio/static/media/JPMorgan.f44eb273.png"
+  },
+  {
+    "revision": "12f3da51707f4b87882d6756cbc21747",
+    "url": "/Portfolio/static/media/JQuery.12f3da51.jpg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/Portfolio/static/media/JavaScript.2d2e31cc.svg"
+  },
+  {
+    "revision": "fd0d5546fdbdc85c76c4372a0d51f1bc",
+    "url": "/Portfolio/static/media/LinkedIn.fd0d5546.png"
+  },
+  {
+    "revision": "eb2c9b4a56ea8cb3c15e49bb48f375e5",
+    "url": "/Portfolio/static/media/MovieFinder.eb2c9b4a.ico"
+  },
+  {
+    "revision": "8749c3a3bbb9008caedfc204163f5024",
+    "url": "/Portfolio/static/media/NEWS.8749c3a3.webp"
+  },
+  {
+    "revision": "4603f0d1d6482913bccc9693c93bcf5a",
+    "url": "/Portfolio/static/media/React.4603f0d1.webp"
+  },
+  {
+    "revision": "5b3e1d61e8e7592d010bf02c3cf97937",
+    "url": "/Portfolio/static/media/SKCF.5b3e1d61.jpg"
+  },
+  {
+    "revision": "f4e3838b839decdd05912e97aa3f3214",
+    "url": "/Portfolio/static/media/Shlok.f4e3838b.jpg"
+  },
+  {
+    "revision": "444f32509ee093b57e44f247b34027cb",
+    "url": "/Portfolio/static/media/Shlok2.444f3250.JPG"
+  },
+  {
+    "revision": "413ec22612d4c1e3378a3b53350b5e60",
+    "url": "/Portfolio/static/media/VKarma.413ec226.png"
+  },
+  {
+    "revision": "1ef6320bf6bfd2a87b869b6d4c1bb4f7",
+    "url": "/Portfolio/static/media/YuMedic.1ef6320b.png"
+  }
+]);
